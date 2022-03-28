5f9670ffe6f588952f43b2878319ca423173a4a4
jaqueline16 <60374998+jaqueline16@users.noreply.github.com>
2022-03-27 21:20:14 -0600

    Prueba

---
+++ bacteriacounter.py
@@ -0,0 +1,57 @@
+tamaño = input("¿Qué tamaño tienen la bacterias? 1:Grande 2:Normal 3:Chica \n")
+forma = input("¿Qué forma tienen las bacterias? 1:Óptima 2:Regular 3:Mala\n")
+unidades = input("¿Qué número de unidades tienen las bacterias? 1:Muy alto 2:Alto 3:Normal\n")
+if ( tamaño== '1' and forma == '1' and unidades=="1" ):
+	print("Las bacterias son no contables") 
+if ( tamaño== '1' and forma == '1' and unidades=="2" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '1' and forma == '1' and unidades=="3" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '1' and forma == '2' and unidades=="1" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '1' and forma == '2' and unidades=="2" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '1' and forma == '2' and unidades=="3" ):
+	print("Las bacterias son no contables") 
+if ( tamaño== '1' and forma == '3' and unidades=="1" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '1' and forma == '3' and unidades=="2" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '1' and forma == '3' and unidades=="3" ):
+	print("Las bacterias son no contables") 
+if ( tamaño== '2' and forma == '1' and unidades=="1" ):
+	print("Las bacterias son contables") 
+if ( tamaño== '2' and forma == '1' and unidades=="2" ):
+	print("Las bacterias son contables")
+if ( tamaño== '2' and forma == '1' and unidades=="3" ):
+	print("Las bacterias son contables")
+if ( tamaño== '2' and forma == '2' and unidades=="1" ):
+	print("Las bacterias son contables")
+if ( tamaño== '2' and forma == '2' and unidades=="2" ):
+	print("Las bacterias son contables")
+if ( tamaño== '2' and forma == '2' and unidades=="3" ):
+	print("Las bacterias son contables") 
+if ( tamaño== '2' and forma == '3' and unidades=="1" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '2' and forma == '3' and unidades=="2" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '2' and forma == '3' and unidades=="3" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '3' and forma == '1' and unidades=="1" ):
+	print("Las bacterias son contables") 
+if ( tamaño== '3' and forma == '1' and unidades=="2" ):
+	print("Las bacterias son contables")
+if ( tamaño== '3' and forma == '1' and unidades=="3" ):
+	print("Las bacterias son contables")
+if ( tamaño== '3' and forma == '2' and unidades=="1" ):
+	print("Las bacterias son contables")
+if ( tamaño== '3' and forma == '2' and unidades=="2" ):
+	print("Las bacterias son contables")
+if ( tamaño== '3' and forma == '2' and unidades=="3" ):
+	print("Las bacterias son contables") 
+if ( tamaño== '3' and forma == '3' and unidades=="1" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '3' and forma == '3' and unidades=="2" ):
+	print("Las bacterias son no contables")
+if ( tamaño== '3' and forma == '3' and unidades=="3" ):
+	print("Las bacterias son no contables")
